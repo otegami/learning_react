44c9530817197510edc7e6353fd939c54f7a43e0
otegami <a.s.takuya1026@gmail.com>
2021-11-17 08:36:44 +0900

    Created base of recipes app

---
+++ chapter5/recipes-app/src/components/Recipe.js
@@ -0,0 +1,14 @@
+import IngredientsList from './IngredientsList'
+import Instructions from './Instructions'
+
+const Recipe = ({ name, ingredients, steps }) => {
+  return (
+    <session id={name.toLowerCase().replace(/ /g, '-')}>
+      <h1>{name}</h1>
+      <IngredientsList list={ingredients} />
+      <Instructions title={'Cooking Instructions'} steps={steps} />
+    </session>
+  )
+}
+
+export default Recipe
