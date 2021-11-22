2b1c4943b55ff6383a3598d6070d0b3337a94cae
otegami <a.s.takuya1026@gmail.com>
2021-11-22 11:23:22 +0900

    Create test application to learn how to use react-window

---
+++ chapter8/react-window/src/components/List.js
@@ -0,0 +1,13 @@
+const List = ({ data = [], renderItem, renderEmpty }) => {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item, i) => (
+        <li key={i}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  )
+}
+
+export default List
