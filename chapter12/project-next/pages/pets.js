4a0e1cb6cd2ef8d5bead45f1eb4acaa06692bfb4
otegami <a.s.takuya1026@gmail.com>
2021-12-04 11:59:05 +0900

    Implemented pets list

--- chapter12/project-next/pages/pets.js
+++ chapter12/project-next/pages/pets.js
@@ -1,9 +1,42 @@
+import fetch from "isomorphic-fetch"
+import { useState, useEffect } from "react"
 import Layout from "../components/Layout"
 
-const Pets = () => {
+const Pets = (props) => {
+  const [pets, setPets] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const getPets = async () => {
+      const res = await fetch(`http://pet-library.moonhighway.com/api/pets`)
+      const data = await res.json()
+
+      return data
+    }
+
+    const load = async () => {
+      setLoading(true)
+      try {
+        const petsArray = await getPets()
+        setPets(petsArray)
+      } catch (err) {
+        throw new Error(`Pets don't exist.`);
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    load()
+  }, [])
+
+  if (loading) { return <div>loading...</div> }
+
   return (
     <Layout>
       <h1>Pets!</h1>
+      <ul>
+        {pets.map((pet) => <li key={pet.id}>{pet.name}</li>)}
+      </ul>
     </Layout>
   )
 }
